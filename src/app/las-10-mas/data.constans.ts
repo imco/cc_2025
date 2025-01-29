e227d025dcdf8ec69b8c48173f43e8ab066803cb
LorenaMondragon <yoha.lorena@hotmail.com>
2025-01-28 23:56:11 -0600

    create top 10 and dynamic links

---
+++ src/app/las-10-mas/data.constans.ts
@@ -0,0 +1,87 @@
+export type TopInfo = {
+  topName: string,
+  titleUrl: string,
+}
+
+export const TopsLists: TopInfo[] = [
+  {
+    topName: "Las 10 carreras con mayor número de profesionistas",
+    titleUrl: "profesionistas"
+  },
+  {
+    topName: "Las 10 carreras con mayor calidad de inversión en universidades privadas",
+    titleUrl: "calidad-inversion"
+  },
+  {
+    topName: "Las 10 carreras con mayor calidad de inversión en universidades públicas",
+    titleUrl: "calidad-inversion-publicas"
+  },
+  {
+    topName: "Las 10 carreras con el mayor porcentaje de mujeres en nuevo ingreso",
+    titleUrl: "porcentaje-ingreso-mujeres"
+  },
+  {
+    topName: "Las 10 carreras con el mayor porcentaje de hombres en nuevo ingreso",
+    titleUrl: "porcentaje-ingreso-hombres"
+  },
+  {
+    topName: "Las 10 carreras más demandadas (porcentaje de aceptación en universidades públicas)",
+    titleUrl: "demanda-publica"
+  },
+  {
+    topName: "Las 10 carreras con mayor porcentaje de mujeres",
+    titleUrl: "porcentaje-mujeres"
+  },
+  {
+    topName: "Las 10 carreras con mayor porcentaje de desempleados",
+    titleUrl: "desempleados"
+  },
+  {
+    topName: "Las 10 carreras con mayor porcentaje de informalidad",
+    titleUrl: "informalidad"
+  },
+  {
+    topName: "Las 10 carreras con mayor porcentaje de jóvenes",
+    titleUrl: "porcentaje-jovenes"
+  },
+  {
+    topName: "Las 10 carreras con más profesionistas con posgrado",
+    titleUrl: "posgrado"
+  },
+  {
+    topName: "Las 10 carreras con mayor matrícula",
+    titleUrl: "matricula"
+  },
+  {
+    topName: "Las 10 carreras con mayor tasa de retorno en universidades públicas",
+    titleUrl: "mayor-tasa-retorno-publicas"
+  },
+  {
+    topName: "Las 10 carreras con mayor tasa de retorno en universidades privadas",
+    titleUrl: "mayor-tasa-retorno-privadas"
+  },
+  {
+    topName: "Las 10 carreras con menor tasa de retorno en universidades públicas",
+    titleUrl: "menor-tasa-retorno-publicas"
+  },
+  {
+    topName: "Las 10 carreras con menor tasa de retorno en universidades privadas",
+    titleUrl: "menor-tasa-retorno-privadas"
+  },
+  {
+    topName: "Las 10 carreras con mayor riesgo",
+    titleUrl: "mayor-riesgo"
+  },
+  {
+    topName: "Las 10 carreras con menor riesgo",
+    titleUrl: "menor-riesgo"
+  },
+  {
+    topName: "Las 10 carreras mejor pagadas",
+    titleUrl: "mejor-pagadas"
+  },
+  {
+    topName: "Las 10 carreras peor pagadas",
+    titleUrl: "peor-pagadas"
+  }
+]
--- src/app/las-10-mas/page.tsx
+++ src/app/las-10-mas/page.tsx
@@ -1,5 +1,39 @@
+import Link from "next/link";
+
+import { TopInfo, TopsLists } from "./data.constans";
+
 export default function Las10Mas() {
   return (
-    <div>Las10Mas</div>
+    <main className="container pt-12">
+      <div className="row">
+        <div className="col-12 ">
+          <div className="flex align-middle justify-center h-16 text-center">
+            <h1 className="text-principal text-4xl font-bold">
+              Top 10
+            </h1>
+          </div>
+        </div>
+        <div className="col-12">
+          <div className="flex align-middle justify-center text-center">
+            <span className="text-principal text-xl font-bold">
+              ¿Cuántos profesionistas tiene cada carrera? ¿cuáles tienen más mujeres y hombres? ¿cuáles ofrecen mejor salario? ¿cuáles carreras tienen una mayor tasa de desempleo? entre otras.
+            </span>
+          </div>
+        </div>
+      </div>
+      <div className="row mt-10 lg:px-12 xl:px-20">
+        {TopsLists.map((top: TopInfo) => (
+          <div className="col-12 col-md-4 mb-4" key={top.titleUrl}>
+            <Link href={"/las-10-mas/" + top.titleUrl}>
+              <div className="card shadow-md rounded-2xl py-3 px-2 lg:h-40 h-52  text-center bg-slate-100 shadow-blue-500/50 text-xl">
+                <div className="flex flex-col align-middle justify-center h-full">
+                  {top.topName}
+                </div>
+              </div>
+            </Link>
+          </div>
+        ))}
+      </div>
+    </main>
   )
 }
