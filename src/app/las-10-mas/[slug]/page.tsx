1348d50e49914dd78e3199e144a57d3538667e2b
LorenaMondragon <yoha.lorena@hotmail.com>
2025-01-29 00:07:41 -0600

    generate static params

--- src/app/las-10-mas/[slug]/page.tsx
+++ src/app/las-10-mas/[slug]/page.tsx
@@ -1,3 +1,12 @@
+export async function generateStaticParams() {
+  const tops = await fetch('https://.../las-10-mas').then((res) => res.json())
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  return tops.map((top: any) => ({
+    slug: top.slug,
+  }))
+}
+
 export default async function Page({ params }: {
   params: Promise<{ slug: string }>
 }) {
