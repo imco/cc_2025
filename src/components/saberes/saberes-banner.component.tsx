4a766b54bf7d70abeb98eb322d4c79391b23490e
jbravo <joel.bravo@imco.org.mx>
2026-02-12 11:39:23 -0600

    Ajuste de tamaño de baner en home

--- src/components/saberes/saberes-banner.component.tsx
+++ src/components/saberes/saberes-banner.component.tsx
@@ -11,10 +11,20 @@ export default function SaberesBanner() {
     >
       <div className="saberes-banner-content">
         <h3 className="saberes-banner-title">
-          Certificaciones para seguir aprendiendo
+          <span className="saberes-banner-title-desktop">
+            Certificaciones para seguir aprendiendo
+          </span>
+          <span className="saberes-banner-title-mobile">
+            Aquí encontrarás certificaciones para seguir aprendiendo
+          </span>
         </h3>
         <p className="saberes-banner-description">
-          Las <strong>microcredenciales</strong> te permiten actualizar tus conocimientos y desarrollar habilidades clave de forma flexible. En <strong>SaberesMx</strong> la plataforma de la Secretaría de Educación Pública puedes obtener certificaciones gratuitas para profundizar en tu área de interés y fortalecer tu desarrollo profesional.
+          <span className="saberes-banner-description-intro">
+            Las <strong>microcredenciales</strong> te permiten actualizar tus conocimientos y desarrollar habilidades clave de forma flexible.
+          </span>{" "}
+          <span>
+            En <strong>SaberesMx</strong> la plataforma de la Secretaría de Educación Pública puedes obtener certificaciones gratuitas para profundizar en tu área de interés y fortalecer tu desarrollo profesional.
+          </span>
         </p>
       </div>
       <div className="saberes-banner-logo-container">
